cc3285bdb6a400d244447ac58e3dbb250a685337
Asim Williams <asim.efb@gmail.com>
2020-03-18 15:55:07 -0400

    Milestone1Issue1: Added Control Panel View Controller

---
+++ Recital/ControlPanelViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ControlPanelViewController.swift
+//  Recital
+//
+//  Created by Asim Williams on 3/18/20.
+//  Copyright © 2020 Asim Williams. All rights reserved.
+//
+
+import UIKit
+
+class ControlPanelViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
