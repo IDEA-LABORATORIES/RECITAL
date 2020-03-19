c086622514146bad6624e3f012c661656a453d1a
Asim Williams <asim.efb@gmail.com>
2020-03-19 17:02:45 -0400

    Milestone1Issue1: Implemented functionality for copying mp3 file from files app to recital sandbox.

--- Recital/ControlPanelViewController.swift
+++ Recital/ControlPanelViewController.swift
@@ -7,8 +7,9 @@
 //
 
 import UIKit
+import MobileCoreServices
 
-class ControlPanelViewController: UIViewController {
+class ControlPanelViewController: UIViewController, UIDocumentMenuDelegate, UIDocumentPickerDelegate, UINavigationControllerDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -16,15 +17,53 @@ class ControlPanelViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    public func documentPicker(_ controller: UIDocumentPickerViewController, didPickDocumentsAt urls: [URL]) {
+        guard let selectedFileURL = urls.first else {
+            return
+        }
+        print("Selected File : \(selectedFileURL)")
+        
+        let dir = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!
+        let sandboxFileURL = dir.appendingPathComponent(selectedFileURL.lastPathComponent)
+        
+        if FileManager.default.fileExists(atPath: sandboxFileURL.path) {
+            print("This file has already been copied.")
+        }
+        else {
+            
+            do {
+                try FileManager.default.copyItem(at: selectedFileURL, to: sandboxFileURL)
+                
+                print("File Copied.")
+            }
+            catch {
+                print("Error: \(error)")
+            }
+        }
     }
-    */
 
+    public func documentMenu(_ documentMenu:UIDocumentMenuViewController, didPickDocumentPicker documentPicker: UIDocumentPickerViewController) {
+        documentPicker.delegate = self
+        present(documentPicker, animated: true, completion: nil)
+    }
+
+    func documentPickerWasCancelled(_ controller: UIDocumentPickerViewController) {
+        print("view was cancelled")
+        dismiss(animated: true, completion: nil)
+    }
+    
+    // https://www.youtube.com/watch?v=p1UNnsodJxc (Dont Work)
+    // https://stackoverflow.com/questions/37296929/implement-document-picker-in-swift-ios (Do Work)
+    // Present the DocumentPickerViewController to select audio file in files application.
+    @IBAction func onSelectAudio(_ sender: Any) {
+        /* https://developer.apple.com/library/archive/documentation/Miscellaneous/Reference/UTIRef/Articles/System-DeclaredUniformTypeIdentifiers.html
+        */
+        let documentPicker = UIDocumentPickerViewController(documentTypes: ["public.mp3"], in: .import)
+        
+        documentPicker.delegate = self
+        documentPicker.allowsMultipleSelection = false
+        
+        documentPicker.modalPresentationStyle = UIModalPresentationStyle.fullScreen
+        present(documentPicker, animated: true, completion: nil)
+    }
 }
