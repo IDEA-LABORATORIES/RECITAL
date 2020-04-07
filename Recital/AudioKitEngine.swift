d0ca4fb21a52679a3145cd5fd8d4707783794c85
Asim Williams <asim.efb@gmail.com>
2020-04-06 22:55:56 -0400

    M1I1: Implement pausing functionality.

--- Recital/AudioKitEngine.swift
+++ Recital/AudioKitEngine.swift
@@ -14,49 +14,43 @@ class AudioKitEngine {
         self.audioSandboxFileURL = audioSandboxFileURL
     }
     
+    var playerStarted: Int = 0
     public func play() {
-        // Its not even getting here the second time
-        playCount += 1
-        print("play: ", playCount)
-        if (playCount > 1) {
-            player.stop()
-            do {
-                try AudioKit.stop()
-            } catch {
-                AKLog("AudioKit did not stop")
+        // TODO: Initialize player stuff somewhere between file being selected and play button being pressed for the first time.
+        if (playerStarted == 0) {
+            // https://github.com/AudioKit/AudioKit/issues/1073
+            // If there is an audio file URL create an audio kit file from it.
+            selectedAudio = try? AKAudioFile(forReading: URL(string: audioSandboxFileURL.absoluteString)!)
+            guard selectedAudio != nil else {
+                print("can't play file")
+                return
             }
+            
+            // AudioKitPlayer node uses audio file as input.
+            player = AKPlayer(audioFile: selectedAudio)
+            player.completionHandler = { AKLog("completion callback has been triggered!") }
+            
+            player.isLooping = true
+            // Sound that comes out speakers is set as player output.
+            AudioKit.output = player
+            
+            
+            
+            do {
+                try AudioKit.start()
+            } catch {
+                AKLog("AudioKit did not start")
+            }
+            playerStarted = 1
         }
         
-        // https://github.com/AudioKit/AudioKit/issues/1073
-        // If there is an audio file URL create an audio kit file from it.
-        selectedAudio = try? AKAudioFile(forReading: URL(string: audioSandboxFileURL.absoluteString)!)
-        guard selectedAudio != nil else {
-            print("can't play file")
-            return
+        if (player.isPaused) {
+            player.play(from: player.pauseTime!)
+        } else {
+            player.play()
         }
-        
-        // AudioKitPlayer node uses audio file as input.
-        player = AKPlayer(audioFile: selectedAudio)
-        player.completionHandler = { AKLog("completion callback has been triggered!") }
-        
-        player.isLooping = true
-        // Sound that comes out speakers is set as player output.
-        AudioKit.output = player
-        
-        
-        
-        do {
-            try AudioKit.start()
-        } catch {
-            AKLog("AudioKit did not start")
-        }
-        player.play()
     }
     
-    /** Pause:
-     let wasPlaying = player.isPlaying
-     if wasPlaying { player.stop() }
-     */
     // Pause
     public func pause() {
         if (player.isPlaying) {
--- Recital/ControlPanelViewController.swift
+++ Recital/ControlPanelViewController.swift
@@ -4,7 +4,7 @@ class ControlPanelViewController: UIViewController {
 
     var audioSelector: AudioSelector!
     var audioKitEngine: AudioKitEngine!
-    var playCount: Int = 0
+//    var playCount: Int = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,17 +19,20 @@ class ControlPanelViewController: UIViewController {
     // TODO: Disable button if audioSandboxFileURL is null.
     // TODO: Changing volume on simulator breaks play.
     var playPauseToggle: Int = 0
-    @IBAction func onPlay(_ sender: Any) {
+    var playerStarted: Int = 0
+    @IBAction func onPlay(_ sender: UIButton) {
         if (playPauseToggle == 0) {
-            if (playCount == 0) {
+            if (playerStarted == 0) {
                 audioKitEngine = AudioKitEngine(audioSandboxFileURL: audioSelector.getAudioSandboxURL())
-                playCount += 1
+                playerStarted = 1
             }
             audioKitEngine.play()
             playPauseToggle = 1;
+            sender.setTitle("Pause", for: .normal)
         } else {
             audioKitEngine.pause()
             playPauseToggle = 0
+            sender.setTitle("Play", for: .normal)
         }
     }
     
