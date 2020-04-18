bb37182304d322bd2436680657f4ce80880feed7
Asim Williams <asim.efb@gmail.com>
2020-04-18 11:50:36 -0400

    M4i4: User can see note values in real time.

--- Recital/AudioKitEngine.swift
+++ Recital/AudioKitEngine.swift
@@ -5,6 +5,8 @@ class AudioKitEngine {
     var player: AKPlayer!
     // Declare Time and Pitch node
     var timeAndPitchManipulator: AKTimePitch!
+    // Declare Frequency Tracking node
+    var frequencyTracker: AKFrequencyTracker!
     
     // Instance Variables
     var audioSandboxFileURL: URL
@@ -29,8 +31,11 @@ class AudioKitEngine {
         // Output from player node (audioplayer) is input for time and pitch maniplator node
         timeAndPitchManipulator = AKTimePitch(player)
         
+        // Output form time and pitch maniplator node is input for frequency tracking node
+        frequencyTracker = AKFrequencyTracker(timeAndPitchManipulator)
+        
         // Sound that comes out speakers is set as timeAndPitchManipulator output.
-        AudioKit.output = timeAndPitchManipulator
+        AudioKit.output = frequencyTracker
         do {
             try AudioKit.start()
         } catch {
@@ -67,6 +72,41 @@ class AudioKitEngine {
     public func toggleLooping(loop: Bool) {
         player.isLooping = loop
     }
+    
+//------------------------------------- Audio Analysis Start ------------------------------
+    let noteFrequencies = [16.35, 17.32, 18.35, 19.45, 20.6, 21.83, 23.12, 24.5, 25.96, 27.5, 29.14, 30.87]
+    let noteNamesWithSharps = ["C", "C♯", "D", "D♯", "E", "F", "F♯", "G", "G♯", "A", "A♯", "B"]
+    let noteNamesWithFlats = ["C", "D♭", "D", "E♭", "E", "F", "G♭", "G", "A♭", "A", "B♭", "B"]
+    
+    public func getFrequency() -> Float {
+        return Float(frequencyTracker.frequency)
+    }
+    
+    public func determineNote() -> Array<String> {
+        let trackerFrequency = Float(frequencyTracker.frequency)
+        var frequency = trackerFrequency
+        while frequency > Float(noteFrequencies[noteFrequencies.count - 1]) {
+            frequency /= 2.0
+        }
+        while frequency < Float(noteFrequencies[0]) {
+            frequency *= 2.0
+        }
+
+        var minDistance: Float = 10_000.0
+        var index = 0
+
+        for i in 0..<noteFrequencies.count {
+            let distance = fabsf(Float(noteFrequencies[i]) - frequency)
+            if distance < minDistance {
+                index = i
+                minDistance = distance
+            }
+        }
+        let octave = Int(log2f(trackerFrequency / frequency))
+        let notes = ["\(noteNamesWithSharps[index])\(octave)", "\(noteNamesWithSharps[index])\(octave)"]
+        return notes
+    }
+//------------------------------------- Audio Analysis End --------------------------------
     
     // Getters
     public func getAudioFileDuration() -> Double {
